b5a92e3005d129c0fc4640bbbd1d4968ac6f2adc
SwaraDeep <swaradeepneeli@gmail.com>
2018-09-20 20:50:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,35 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.android.pets.data.PetsEntries.*;
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "shelter.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        Log.v("SQL_CREATE:", SQL_CREATE_PETS_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+}
